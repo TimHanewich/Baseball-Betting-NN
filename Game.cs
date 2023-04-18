b75bfd0b60a8ee34614e07dbd412fbbac376ef97
Tim Hanewich <tahanewich@live.com>
2023-04-17 20:38:34 -0400

    State

--- Game.cs
+++ Game.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace ESPN
 {
@@ -13,7 +15,7 @@ namespace ESPN
         public int HomeTeamHits {get; set;}
         public int AwayTeamErrors {get; set;}
         public int HomeTeamErrors {get; set;}
-        public int Inning {get; set;}
+        public int Inning {get; set;} //If the inning is 0, it means the game either hasn't started yet or is over
         public Team BattingTeam {get; set;}
         public int Outs {get; set;}
         public int Balls {get; set;}
@@ -21,5 +23,48 @@ namespace ESPN
         public bool ManOnFirst {get; set;}
         public bool ManOnSecond {get; set;}
         public bool ManOnThird {get; set;}
+
+        public float[] ToState()
+        {
+            List<float> ToReturn = new List<float>();
+
+            ToReturn.Add(AwayTeamWinningRecord);
+            ToReturn.Add(HomeTeamWinningRecord);
+            ToReturn.Add(AwayTeamRuns);
+            ToReturn.Add(HomeTeamRuns);
+            ToReturn.Add(AwayTeamHits);
+            ToReturn.Add(HomeTeamHits);
+            ToReturn.Add(AwayTeamErrors);
+            ToReturn.Add(HomeTeamErrors);
+            ToReturn.Add(Inning);
+            ToReturn.Add(Convert.ToSingle(BattingTeam));
+            ToReturn.Add(Outs);
+            ToReturn.Add(Balls);
+            ToReturn.Add(Strikes);
+            ToReturn.Add(Convert.ToInt32(ManOnFirst));
+            ToReturn.Add(Convert.ToInt32(ManOnSecond));
+            ToReturn.Add(Convert.ToInt32(ManOnThird));
+
+            return ToReturn.ToArray();
+        }
+
+        public static bool EquivalentStates(float[] s1, float[] s2)
+        {
+            if (s1.Length != s2.Length)
+            {
+                return false;
+            }
+            else
+            {
+                for (int t = 0; t < s1.Length; t++)
+                {
+                    if (s1[t] != s2[t])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace ESPN
 {
@@ -8,7 +9,22 @@ namespace ESPN
     {
         public static void Main(string[] args)
         {
-            Scoreboard.RetrieveAsync().Wait();
+
+            float[] f1 = new float[]{0.4f, 0.5f, 0.3f};
+            float[] f2 = new float[]{0.4f, 0.5f, 0.3f};
+            Console.WriteLine(Game.EquivalentStates(f1, f2));
+            Console.ReadLine();
+
+            
+            Scoreboard s = Scoreboard.RetrieveAsync().Result;
+            foreach (Game g in s.Games)
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(g, Formatting.Indented));
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+            
 
         }
     }
