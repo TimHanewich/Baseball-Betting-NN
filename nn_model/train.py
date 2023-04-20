36e67ee20f6797b7ce99d2f865077fb0edf2518a
Tim Hanewich <tahanewich@live.com>
2023-04-20 09:27:22 -0400

    Save to new folder

--- nn_model/train.py
+++ nn_model/train.py
@@ -58,4 +58,4 @@ model.fit(i, o, epochs=350, verbose=True)
 print("complete!")
 
 # save
-model.save(r"C:\Users\timh\Downloads\tah\nn\nn_model\models\model4")
+model.save(r"C:\Users\timh\Downloads\tah\nn\nn_model\models\model5")
