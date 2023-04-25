2d4b1bec2ea65dd5a2c0bea8f0a93c93fe1ac125
Tim Hanewich <tahanewich@live.com>
2023-04-25 10:38:11 -0400

    Use model7

--- nn_model/ui.py
+++ nn_model/ui.py
@@ -1,5 +1,5 @@
 ##### SETTINGS #####
-nn_model_path = r"C:\Users\timh\Downloads\tah\nn\nn_model\models\model6"
+nn_model_path = r"C:\Users\timh\Downloads\tah\nn\models\model7"
 ####################
 
 
